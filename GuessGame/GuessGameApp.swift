253febf1a412449ab93f3c25ccadd09dfed65c8f
Igor Gadomskii <igvgad@gmail.com>
2022-05-13 00:42:49 +0300

    Initial Commit

---
+++ GuessGame/GuessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessGameApp.swift
+//  GuessGame
+//
+//  Created by Macbook on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
